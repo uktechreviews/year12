cce0988f7817bebd1f9c7b43296bd5c2dca77bac
uktechreviews <spencerorgan1@me.com>
2024-10-07 15:56:52 +0100

    new project

---
+++ twentyone.py
@@ -0,0 +1,9 @@
+## The counting game 
+## The objective is get to 21
+print ("The counting to 21 game")
+print ("--- -------- -- -- ----")
+print ("\n Rules")
+print ("\n")
+print ("1. Player 1 starts with the number 1, you can then say either 1 or 2")
+print ("2. Player 2 starts with the next number after player 1 and then can their say +1 or +2")
+print ("3. Repeat until you get to 21")
