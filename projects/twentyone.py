08a6d9b1474d47177db3ccf78cb29be9bd80777d
uktechreviews <spencerorgan1@me.com>
2024-10-07 17:30:58 +0100

    completed code

--- projects/twentyone.py
+++ projects/twentyone.py
@@ -7,3 +7,45 @@ print ("\n")
 print ("1. Player 1 starts with the number 1, you can then say either 1 or 2")
 print ("2. Player 2 starts with the next number after player 1 and then can their say +1 or +2")
 print ("3. Repeat until you get to 21")
+
+win = False
+total = 0
+
+def checkValid(number):
+    if number >=1 and number <=2:
+        return True
+    else:
+        return False
+    
+def checkWin(total):
+    if total == 21:
+        return True
+    else:
+        return False
+
+while win != True:
+    player1 = int(input("Player 1: Enter a number between 1 and 2 "))
+    if checkValid(player1) == True:
+        total = total + player1
+        print (f"The total is {total}")
+        if checkWin(total) == True:
+            print ("Player 1 Wins")
+            winner = "player 1"
+            win = True
+            break
+    else:
+        print ("Not a valid number - you forefit yor go")
+
+    player2 = int(input("Player 2: Enter a number between 1 and 2 "))
+    if checkValid(player2) == True:
+        total = total + player2
+        print (f"The total is {total}")
+        if checkWin(total) == True:
+            print ("Player 2 Wins")
+            win = True
+            winner = "player 2"
+    else:
+        print ("Not a valid number - you forefit your go")
+
+print (f"Game Over! - Well done {winner}")
+    
